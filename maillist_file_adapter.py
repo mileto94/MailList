a90093895716dbafcef652e6bbfb8e4a6ddef1b8
Emilian <emilian@emilian-ThinkPad-T61.(none)>
2014-03-27 16:05:38 +0200

    Revert changes

--- maillist_file_adapter.py
+++ maillist_file_adapter.py
@@ -1,15 +1,19 @@
 from maillist import MailList
 import os
-import sqlite3
 
 
 class MailListFileAdapter():
     """docstring for MailListFileAdapter"""
     def __init__(self, db_path, mail_list=None):
-        self.db_path = sqlite3.connect("maillist.db")
-        self.cursor = self.db_path.cursor()
+        self.db_path = db_path
         self.mail_list = mail_list
         self._ensure_db_path()
+
+    def get_file_name(self):
+        return self.mail_list.get_name().replace(" ", "_")
+
+    def get_file_path(self):
+        return self.db_path + self.get_file_name()
 
     # (name, email) -> "<name> - <email>"
     def prepare_for_save(self):
@@ -19,14 +23,12 @@ class MailListFileAdapter():
         return sorted(subscribers)
 
     def save(self):
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS Lists(id, name)""")
-        lists_query = ("""INSERT INTO Lists(id, name) VALUES(?, ?)""")
-        lists_data = [self.mail_list.get_id(), self.mail_list.get_name()]
-        self.cursor.execute(lists_query, lists_data)
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS
-                    {}(id, name, email)""".format(self.mail_list.get_name()))
-        self.db_path.commit()
-        self.db_path.close()
+        file_to_save = open(self.get_file_path(), "w")
+        contents = "{}\n".format(self.mail_list.get_id())
+        contents += "\n".join(self.prepare_for_save())
+
+        file_to_save.write(contents)
+        file_to_save.close()
 
     def load(self, file_name):
         maillist_name = file_name.replace("_", " ")
@@ -55,5 +57,5 @@ class MailListFileAdapter():
         return result
 
     def _ensure_db_path(self):
-        if not os.path.isfile("maillist.db"):
-            self.db_path = sqlite3.connect("maillist.db")
+        if not os.path.exists(self.db_path):
+            os.makedirs(self.db_path)
