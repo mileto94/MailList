de26c22933c0835f5ec1d4fa5693080267308a89
milka <mileto94@abv.bg>
2014-03-28 22:37:13 +0200

    Make some unneeded tests part of docstring

--- test_maillist.py
+++ test_maillist.py
@@ -19,27 +19,28 @@ class MailListTest(unittest.TestCase):
         self.m.add_subscriber("Rado", "radorado@hackbulgaria.com")
         expected = [("Rado", "radorado@hackbulgaria.com")]
         self.assertEqual(expected, self.m.get_subscribers())
+"""
+    def test_get_id(self):
+        self.assertEqual(1, self.m.get_id())
 
-    # def test_get_id(self):
-    #     self.assertEqual(1, self.m.get_id())
+    def test_get_id_after_three_instances(self):
+        m1 = MailList(1, "Hack")
+        m2 = MailList(2, "Hack")
+        m3 = MailList(3, "Hack")
+        self.assertEqual(1, m1.get_id())
+        self.assertEqual(2, m2.get_id())
+        self.assertEqual(3, m3.get_id())
 
-    # def test_get_id_after_three_instances(self):
-    #     m1 = MailList(1, "Hack")
-    #     m2 = MailList(2, "Hack")
-    #     m3 = MailList(3, "Hack")
-    #     self.assertEqual(1, m1.get_id())
-    #     self.assertEqual(2, m2.get_id())
-    #     self.assertEqual(3, m3.get_id())
-
-    # def test_add_subscriber_with_same_email_address(self):
-    #     subscriber_email = "radorado@hackbulgaria.com"
-    #     add1 = self.m.add_subscriber("Rado", subscriber_email)
-    #     add2 = self.m.add_subscriber("Rado Rado", subscriber_email)
-    #     self.assertEqual(1, self.m.count())
-    #     self.assertTrue(add1)
-    #     self.assertFalse(add2)
-    #     self.assertEqual(("Rado", subscriber_email),
-    #                      self.m.get_subscriber_by_email(subscriber_email))
+    def test_add_subscriber_with_same_email_address(self):
+        subscriber_email = "radorado@hackbulgaria.com"
+        add1 = self.m.add_subscriber("Rado", subscriber_email)
+        add2 = self.m.add_subscriber("Rado Rado", subscriber_email)
+        self.assertEqual(1, self.m.count())
+        self.assertTrue(add1)
+        self.assertFalse(add2)
+        self.assertEqual(("Rado", subscriber_email),
+                         self.m.get_subscriber_by_email(subscriber_email))
+"""
 
     def test_add_get_subscriber_by_email(self):
         self.m.add_subscriber("Rado", "radorado@hackbulgaria.com")
